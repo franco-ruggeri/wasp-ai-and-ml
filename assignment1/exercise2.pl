02beb603d544c8e8b3f0cde8be0b234377667ed3
Franco Ruggeri <franco.ruggeri@ericsson.com>
2023-04-12 19:24:57 +0200

    Optimize exercise 2
    Updated definition of highcard to avoid linear complexity. Now, the tests run successfully super quickly.

--- assignment1/exercise2.pl
+++ assignment1/exercise2.pl
@@ -1,6 +1,5 @@
 %%%% Implement the following predicates.
 
-:- use_module(library(apply)).
 :- use_module(library(lists)).
 
 % Rank values
@@ -9,13 +8,12 @@ value(queen,12).
 value(king,13).
 value(ace,14).
 
-/*
 % High card
-hand(Cards,highcard(Rank)) :-
-    maplist(value,Cards,CardValues),
-    max_member(RankValue,CardValues),
-    value(Rank,RankValue).
-*/
+% Important: we define highcard as any of the cards, not as the highest card.
+% Indeed, we are just interested in finding the best hand, so the lower cards will not impact the result.
+% Searching for the highest card would would add a linear complexity (finding max), resulting in a very high
+% computation time (in the online editor, even an out of time error).
+hand(Cards,highcard(Rank)) :- member(Rank,Cards).
 
 % One pair
 hand(Cards,onepair(Rank)) :-
@@ -28,7 +26,7 @@ hand(Cards,twopair(Rank1,Rank2)) :-
     hand(Cards,onepair(Rank2)),
     value(Rank1,RankValue1),
     value(Rank2,RankValue2),
-    RankValue1 > RankValue2.        % highest rank is always in the first position, easier to implement better()
+    RankValue1 > RankValue2.        % highest rank is always in the first position, easier to implement value()
 
 % Three of a kind
 hand(Cards,threeofakind(Rank)) :-
@@ -38,7 +36,7 @@ hand(Cards,threeofakind(Rank)) :-
 
 % Straight
 % Important: we define straight only a hand containing [ace,king,queen,jack].
-% A more general definition would be to have sequential cards, independently of the number and of the highest rank.
+% A more general definition would be to have sequential cards, independently of the number of cards.
 % However, the following test in test.py does not consider [jack,king,queen] a straight. Thus, the general definition
 % would fail the test.
 % game_outcome(list2term([jack, king, ace]), list2term([jack, king, queen]), X): [({X: player1}, 1.0)],
@@ -49,6 +47,9 @@ hand(Cards,straight) :-
     member(jack,Cards).
 
 % Hand values
+% Arguments: (hand, category_value, rank_value)
+% - category_value (second argument) is useful to compare different hand categories (e.g., one pair vs two pair).
+% - rank_value (third argument) is useful to compare same hand category (e.g., one pair) with different ranks.
 value(highcard(Rank),0,RankValue) :- value(Rank,RankValue).
 value(onepair(Rank),1,RankValue) :- value(Rank,RankValue).
 value(twopair(Rank1,Rank2),2,RankValue) :-
