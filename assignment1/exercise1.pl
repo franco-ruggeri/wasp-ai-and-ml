ed143355a0a1d1af93cd9bb0263b3a9872291b66
Franco Ruggeri <franco.ruggeri@ericsson.com>
2023-04-13 16:39:50 +0200

    Minor changes in exercise1

--- assignment1/exercise1.pl
+++ assignment1/exercise1.pl
@@ -6,16 +6,21 @@ value(queen,12).
 value(king,13).
 value(ace,14).
 
+% Cheater
 1/5::cheater.
 % t(_)::cheater.      % to learn the probability, replace the previous line with this one
 
+% Draw1
 1/4::draw1(jack); 1/4::draw1(queen); 1/4::draw1(king); 1/4::draw1(ace).
 
+% Draw2
 1/4::draw2(queen); 1/4::draw2(king); 2/4::draw2(ace) :- cheater.
 1/4::draw2(jack); 1/4::draw2(queen); 1/4::draw2(king); 1/4::draw2(ace) :- \+cheater.
 
+% Coin
 1/2::coin(heads) :- \+cheater.
 
+% Highest
 highest(none) :- draw1(Card), draw2(Card).          % players have the same card
 highest(player1) :-
     draw1(Card1),
@@ -25,6 +30,7 @@ highest(player1) :-
     Value1 > Value2.                                % player 1 has the highest card
 highest(player2) :- \+highest(player1), \+highest(none).
 
+% Winner
 winner :- highest(player1).
 winner :- highest(none), coin(heads).
 
